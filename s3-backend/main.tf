e24d16be542976d3fa86d015f083df6e97de0269
berotrip <berolina@me.com>
2023-01-19 11:38:00 +0100

    updated versioning, encryption and access
    Prevents the message
    Warning: Argument is deprecated
    
    New configuration for Terraform adjusted for bucket.

--- s3-backend/main.tf
+++ s3-backend/main.tf
@@ -31,23 +31,29 @@ locals {
 resource "aws_s3_bucket" "terraform_state" {
   # With account id, this S3 bucket names can be *globally* unique.
   bucket = "${local.account_id}-terraform-states"
+}
 
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.bucket
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
   }
 }
 
+# Enable versioning so we can see the full revision history of our
+# state files
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Disable public access to bucket
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
